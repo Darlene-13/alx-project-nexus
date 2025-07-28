d879f821f0ce32bef7c9eb3807c08b726ebbdfe3
Darlene-13 <darlenenasimiyu@gmail.com>
2025-07-28 23:28:49 +0300

    Update settings.py to use os.getenv for environment variables

--- movie_recommendation_backend/movie_recommendation_backend/settings.py
+++ movie_recommendation_backend/movie_recommendation_backend/settings.py
@@ -27,14 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('MOVIE_RECOMMENDATION_BACKEND_SECRET')
-
+SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 # USER MODEL
 AUTH_USER_MODEL = 'authentication.User'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver','0.0.0.0', '*.onrender.com'].split(',')
 
 # Application definition
 
