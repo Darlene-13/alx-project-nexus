ea10cf74c0904bb63b15bbea5e145e539b9478df
Darlene-13 <darlenenasimiyu@gmail.com>
2025-07-29 14:05:46 +0300

    Updated seed_users.py

--- movie_recommendation_backend/movie_recommendation_backend/seed_users.py
+++ movie_recommendation_backend/movie_recommendation_backend/seed_users.py
@@ -2,9 +2,11 @@ import os
 import django
 import random
 import string
+import sys
 from django.utils import timezone
 
 # Set up Django environment
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_recommendation_backend.settings')
 django.setup()
 
