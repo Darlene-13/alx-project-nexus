0c6e129173af11eede07b401eff6af58cc8397e1
Darlene-13 <darlenenasimiyu@gmail.com>
2025-07-29 13:50:25 +0300

    Feat: Added seed_users.py

--- movie_recommendation_backend/entrypoint.sh
+++ movie_recommendation_backend/entrypoint.sh
@@ -2,10 +2,15 @@
 
 # Optional: run database migrations if RUN_MIGRATIONS is true
 if [ "$RUN_MIGRATIONS" = "true" ]; then
-    echo "🟡 Running migrations..."
+    echo "Running migrations..."
     python3 manage.py migrate --noinput
 fi
 
+# Seed the database with users
+echo "📦 Seeding users..."
+python3 movie_recommendation_backend/seed_users.py
+
+
 # Start the app normally
-echo "🟢 Starting server..."
+echo "Starting server..."
 exec gunicorn movie_recommendation_backend.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 120 --log-level info
