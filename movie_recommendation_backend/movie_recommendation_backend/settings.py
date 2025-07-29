6cfe82b932c40c84dd9325feee0efef29bee8bd7
Darlene-13 <darlenenasimiyu@gmail.com>
2025-07-29 11:36:18 +0300

    Doc: Updated Settings.py and requirements.txt

--- movie_recommendation_backend/movie_recommendation_backend/settings.py
+++ movie_recommendation_backend/movie_recommendation_backend/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 from datetime import timedelta
+import dj_database_url
 
 
 load_dotenv() 
@@ -27,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
+SECRET_KEY = os.getenv('MOVIE_RECOMMENDATION_BACKEND_SECRET', 'your-default-secret-key')
 # USER MODEL
 AUTH_USER_MODEL = 'authentication.User'
 # SECURITY WARNING: don't run with debug turned on in production!
