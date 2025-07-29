4f45704c5e69667a3246dd8a7cbdf76965230d74
Darlene-13 <darlenenasimiyu@gmail.com>
2025-07-29 12:46:45 +0300

    Final Dockerfile with entrypoint and migration automation

---
+++ movie_recommendation_backend/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Optional: run database migrations if RUN_MIGRATIONS is true
+if [ "$RUN_MIGRATIONS" = "true" ]; then
+    echo "🟡 Running migrations..."
+    python3 manage.py migrate --noinput
+fi
+
+# Start the app normally
+echo "🟢 Starting server..."
+exec gunicorn movie_recommendation_backend.wsgi:application --bind 0.0.0.0:$PORT
