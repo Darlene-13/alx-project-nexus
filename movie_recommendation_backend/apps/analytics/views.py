97f75a8042fd8caac15aee5b59f7f7f408a9cf30
Darlene-13 <darlenenasimiyu@gmail.com>
2025-08-07 11:04:27 +0300

    Initial commit: Adjusted the analytics views

--- movie_recommendation_backend/apps/analytics/views.py
+++ movie_recommendation_backend/apps/analytics/views.py
@@ -12,7 +12,6 @@ from .serializers import (
     BulkActivityLogSerializer,
     PopularityMetricsSerializer,
     TrendingMoviesSerializer,
-    ActivityAnalyticsSerializer,
     UserActivitySummarySerializer,
     SessionAnalyticsSerializer,
 )
